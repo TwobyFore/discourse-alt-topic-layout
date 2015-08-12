33fe6e1cd026a56ba9643f6d4f1584c45de946af
Christopher Heald <cheald@ashoka.org>
2015-08-12 12:05:50 -0700

    First commit

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: Alternate Topic Layout
+# about: Reorder title, category slug, and topic map in a topic view
+# version: 1.0
+# authors: Christopher Heald
+# url: https://github.com/OnceWas/alt-topic-layout
+
+register_asset "/jsapp/templates/topic.hbs"
+register_asset "/app/assets/javascripts/discourse/templates/topic.hbs"
