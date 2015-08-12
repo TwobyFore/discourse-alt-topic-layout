20cda998ac31787ee0a3ffd7462974ecb4bf1c8c
Christopher Heald <cheald@ashoka.org>
2015-08-12 15:36:33 -0700

    Updated template paths.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 # about: Reorder title, category slug, and topic map in a topic view
 # version: 1.0
 # authors: Christopher Heald
-# url: https://github.com/OnceWas/alt-topic-layout
+# url: https://github.com/OnceWas/discourse-alt-topic-layout
 
-register_asset "/jsapp/templates/topic.hbs"
-register_asset "/app/assets/javascripts/discourse/templates/topic.hbs"
+register_asset "jsapp/templates/topic.hbs"
+register_asset "jsapp/templates/post.hbs"
